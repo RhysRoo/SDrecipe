da9429404bd105082e1f0d8998923cb439443057
Joshua Varney <up2138017@myport.ac.uk>
2024-02-01 20:51:39 +0000

    -a

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -1,18 +1,24 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_log/firebase_options.dart';
 import 'package:flutter_log/pages/auth_page.dart';
-import 'firebase_options.dart';
 
-// Run Section
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(const MyApp());
+  } catch (e) {
+    print('Error initializing Firebase: $e');
+    // Handle the error, for example, show an error message or exit the app.
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
