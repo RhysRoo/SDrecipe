070f7c30db8089546ab4fe14213a95ed2bf0d9de
josh2 <jrv123756@gmail.com>
2024-02-20 16:41:10 +0000

    Carrier Functions, Ingredient Manager, API file, Slider Width Changes

--- recipe_app/lib/pages/add_remove_ingredients_page/add_remove_ingredients_page.dart
+++ recipe_app/lib/pages/add_remove_ingredients_page/add_remove_ingredients_page.dart
@@ -8,9 +8,10 @@ class AddRemoveIngredients extends StatefulWidget {
 }
 
 class _AddRemoveIngredientsState extends State<AddRemoveIngredients> {
-  List<String> ingredients = [];
+  List<List<String>> ingredients = [];
 
   final TextEditingController ingredientController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +21,40 @@ class _AddRemoveIngredientsState extends State<AddRemoveIngredients> {
         title: const Text('Add and Remove Ingredients Page'),
       ),
       backgroundColor: Colors.green[200],
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Card(
-          elevation: 5.0,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _buildIngredientForm(),
-                const SizedBox(height: 20.0),
-                _buildIngredientsList(),
-              ],
-            ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Flush Button
+                  },
+                  child: const Text('Flush'),
+                ),
+              ),
+              Card(
+                elevation: 5.0,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      _buildIngredientForm(),
+                      const SizedBox(height: 20.0),
+                      _buildIngredientsList(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -47,11 +68,20 @@ class _AddRemoveIngredientsState extends State<AddRemoveIngredients> {
           child: TextField(
             controller: ingredientController,
             decoration: const InputDecoration(
-              hintText: 'Enter ingredient...',
+              hintText: 'Enter ingredient name',
             ),
           ),
         ),
-        const SizedBox(width: 10.0),
+        const SizedBox(width: 20.0),
+        Expanded(
+          child: TextField(
+            controller: weightController,
+            decoration: const InputDecoration(
+              hintText: 'Enter grams',
+            ),
+          ),
+        ),
+        const SizedBox(width: 20.0),
         ElevatedButton(
           onPressed: () {
             _addIngredient();
@@ -66,10 +96,6 @@ class _AddRemoveIngredientsState extends State<AddRemoveIngredients> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text(
-          'Ingredients:',
-          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-        ),
         const SizedBox(height: 10.0),
         ListView.builder(
           shrinkWrap: true,
@@ -79,7 +105,8 @@ class _AddRemoveIngredientsState extends State<AddRemoveIngredients> {
               elevation: 3.0,
               margin: const EdgeInsets.symmetric(vertical: 5.0),
               child: ListTile(
-                title: Text(ingredients[index]),
+                title: Text(
+                    '${ingredients[index][0]} : ${ingredients[index][1]} g'),
                 trailing: IconButton(
                   icon: const Icon(Icons.delete),
                   onPressed: () {
@@ -95,10 +122,12 @@ class _AddRemoveIngredientsState extends State<AddRemoveIngredients> {
   }
 
   void _addIngredient() {
-    final newIngredient = ingredientController.text.trim();
-    if (newIngredient.isNotEmpty) {
+    final newIngredient = ingredientController.text.trim(); // String
+    final newIngredientWeight =
+        weightController.text.trim(); // String converted from integer
+    if (newIngredient.isNotEmpty && newIngredientWeight.isNotEmpty) {
       setState(() {
-        ingredients.add(newIngredient);
+        ingredients.add([newIngredient, newIngredient]);
         ingredientController.clear();
       });
     }
@@ -110,4 +139,3 @@ class _AddRemoveIngredientsState extends State<AddRemoveIngredients> {
     });
   }
 }
- 
---
+++ recipe_app/lib/pages/add_remove_ingredients_page/ingredientManager.dart
@@ -0,0 +1,19 @@
+void _getUserIngredients() {
+  // Obtains List from Cloud
+}
+
+void _flushUserIngredients() {
+  // Concatenate New List with Cloud
+}
+
+void pushUserIngredients() {
+  // Send List to Cloud
+}
+
+void adequateIngredients() {
+  // Check that the obtained grams is adequate for a recipe
+}
+
+void checkIngredientisValid() {
+  // Use and API to check the validity of stored information
+}
