66da23585c7915a473525fc226741245512b1f79
josh2 <jrv123756@gmail.com>
2024-02-21 23:08:44 +0000

    Making Soft Changes to Profile Page

--- recipe_app/lib/pages/profile_page/userManager.dart
+++ recipe_app/lib/pages/profile_page/userManager.dart
@@ -2,6 +2,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_log/pages/profile_page/userModel.dart';
 
 class UserManager {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -20,6 +21,46 @@ class UserManager {
     } catch (e) {
       print('Error: $e');
       return ''; // Handle errors by returning an empty string or another default value
+    }
+  }
+
+  Future<Map> storeUserDetails(UserModel user) async {
+    String uid = await getCurrentUserUID();
+
+    try {
+      DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+          await firestore.collection("UserDetails").doc(uid).get();
+
+      if (documentSnapshot.exists) {
+        // Return user details as a Map
+        return documentSnapshot.data()!;
+      } else {
+        // Return an empty Map or handle the case when the document doesn't exist
+        return {};
+      }
+    } catch (e) {
+      print("Error retrieving user details: $e");
+      // Handle errors by returning an empty Map or another default value
+      return {};
+    }
+  }
+
+  Future<Map<String, dynamic>> getUserDetails(String uid) async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> documentSnapshot =
+          await firestore.collection("UserDetails").doc(uid).get();
+
+      if (documentSnapshot.exists) {
+        // Return user details as a Map
+        return documentSnapshot.data()!;
+      } else {
+        // Return an empty Map or handle the case when the document doesn't exist
+        return {};
+      }
+    } catch (e) {
+      print("Error retrieving user details: $e");
+      // Handle errors by returning an empty Map or another default value
+      return {};
     }
   }
 }
