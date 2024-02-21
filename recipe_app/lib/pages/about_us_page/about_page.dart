752c87878976ea5511f64abce34c52043d9b8860
hazaloid <up2120303@myport.ac.uk>
2024-02-21 03:10:01 +0000

    Committing changes to hazaloid2 branch

--- recipe_app/lib/pages/about_us_page/about_page.dart
+++ recipe_app/lib/pages/about_us_page/about_page.dart
@@ -9,7 +9,6 @@ import 'package:flutter_log/pages/recipe_generation_page/generation_page.dart';
 class AboutUsPage extends StatelessWidget {
   AboutUsPage({Key? key}) : super(key: key);
 
-
   // Firebase Authentication instance
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -37,18 +36,17 @@ class AboutUsPage extends StatelessWidget {
     }
   }
 
-
-void _launchEmail2() async {
-  final Uri _emailLaunchUri = Uri(
-    scheme: 'mailto',
-    path: 'up2138017@myport.ac.uk',
-  );
-  if (await canLaunch(_emailLaunchUri.toString())) {
-    await launch(_emailLaunchUri.toString());
-  } else {
-    print('Could not launch email');
+  void _launchEmail2() async {
+    final Uri _emailLaunchUri = Uri(
+      scheme: 'mailto',
+      path: 'up2138017@myport.ac.uk',
+    );
+    if (await canLaunch(_emailLaunchUri.toString())) {
+      await launch(_emailLaunchUri.toString());
+    } else {
+      print('Could not launch email');
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -182,68 +180,68 @@ void _launchEmail2() async {
         ),
       ),
       backgroundColor: Colors.green[200],
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.03,
-              ),
-              Row(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.2,
-                  ),
-                ],
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.009),
-              Container(
-                // Ui Panel
-                height: MediaQuery.of(context).size.height * 0.85,
-                width: MediaQuery.of(context).size.width * 0.95,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.teal[600],
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
                 ),
-                padding: const EdgeInsets.all(16),
-                child: Column(children: [
-                  Text(
-                    'Sustainability is an exciting new application designed to help cut down on food waste. In the United Kingdom, approximately 9.5 million tonnes of food go to waste each year, a staggering amount. Meanwhile, 8.4 million people in the UK struggle with food poverty. It’s a heartbreaking paradox: while so much food is discarded, many individuals and families are left without enough to eat.\n\n'
-                    'Our aim is to significantly reduce the amount of food waste being produced. This is beneficial to both the environment and saving money.\n\n'
-                    'Less wasted food means fewer resources used in production. Further environmental benefits include reducing Greenhouse Gas Emissions, Rotting food in landfills produces methane, a potent greenhouse gas. Cutting waste helps mitigate climate change. Wasting less food means more money in our pockets.\n\n'
-                    'The WasteAway application allows you to enter ingredients that you need to use before their “use by date” expires. Simply enter the ingredients you have into the App and you will be guided to exciting recipes that use what you have available. No more chucking out that “half a pineapple” or “pack of cream cheese”. This is the App that every household needs.',
-                    style: TextStyle(fontSize: 12),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                    ),
+                  ],
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.009),
+                Container(
+                  // Ui Panel
+                  height: MediaQuery.of(context).size.height * 1.2,
+                  width: MediaQuery.of(context).size.width * 0.95,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.teal[600],
                   ),
-                  SizedBox(height: 16),
+                  padding: const EdgeInsets.all(16),
+                  child: Column(children: [
+                    Text(
+                      'Sustainability is an exciting new application designed to help cut down on food waste. In the United Kingdom, approximately 9.5 million tonnes of food go to waste each year, a staggering amount. Meanwhile, 8.4 million people in the UK struggle with food poverty. It’s a heartbreaking paradox: while so much food is discarded, many individuals and families are left without enough to eat.\n\n'
+                      'Our aim is to significantly reduce the amount of food waste being produced. This is beneficial to both the environment and saving money.\n\n'
+                      'Less wasted food means fewer resources used in production. Further environmental benefits include reducing Greenhouse Gas Emissions, Rotting food in landfills produces methane, a potent greenhouse gas. Cutting waste helps mitigate climate change. Wasting less food means more money in our pockets.\n\n'
+                      'The WasteAway application allows you to enter ingredients that you need to use before their “use by date” expires. Simply enter the ingredients you have into the App and you will be guided to exciting recipes that use what you have available. No more chucking out that “half a pineapple” or “pack of cream cheese”. This is the App that every household needs.',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    SizedBox(height: 16),
 
-                  // Add the red containers here
-                 
-                 
-                 
-  Row(
+
+
+Row(
   mainAxisAlignment: MainAxisAlignment.center,
   children: [
     Container(
       width: MediaQuery.of(context).size.width * 0.4,
-      height: MediaQuery.of(context).size.height * 0.25,
+      height: MediaQuery.of(context).size.height * 0.55,
       margin: EdgeInsets.symmetric(vertical: 10),
       decoration: BoxDecoration(
         color: Colors.lightGreen[900],
         borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.black), // Black outline
       ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
         children: [
           Container(
-      width: MediaQuery.of(context).size.width * 0.15,
-      height: MediaQuery.of(context).size.height * 0.15,
+            width: MediaQuery.of(context).size.width * 0.4,
+            height: MediaQuery.of(context).size.height * 0.3,
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('images/about_page/lil_tjay.png'),
                 fit: BoxFit.cover,
               ),
               borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black), // Black outline for image
             ),
           ),
           Padding(
@@ -251,6 +249,20 @@ void _launchEmail2() async {
             child: Text(
               'Josh Varney',
               style: TextStyle(fontSize: 14, color: Colors.white),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+            child: Text(
+              'Developer',
+              style: TextStyle(fontSize: 12, color: Colors.white),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 8.0),
+            child: Text(
+              'I am josh, I helped develope this app, enjoy our application feel free to email me about it.',
+              style: TextStyle(fontSize: 12, color: Colors.white),
             ),
           ),
           Padding(
@@ -269,25 +281,26 @@ void _launchEmail2() async {
     SizedBox(width: 20),
     Container(
       width: MediaQuery.of(context).size.width * 0.4,
-      height: MediaQuery.of(context).size.height * 0.25,
+      height: MediaQuery.of(context).size.height * 0.55,
       margin: EdgeInsets.symmetric(vertical: 10),
       decoration: BoxDecoration(
         color: Colors.lightGreen[900],
         borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.black), // Black outline
       ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
         children: [
           Container(
-            width: MediaQuery.of(context).size.width * 0.15,
-      height: MediaQuery.of(context).size.height * 0.15,
+            width: MediaQuery.of(context).size.width * 0.4,
+            height: MediaQuery.of(context).size.height * 0.3,
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('images/about_page/lil_tjay.png'),
                 fit: BoxFit.cover,
               ),
               borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black), // Black outline for image
             ),
           ),
           Padding(
@@ -295,6 +308,20 @@ void _launchEmail2() async {
             child: Text(
               'Hazaloid Jenkins',
               style: TextStyle(fontSize: 14, color: Colors.white),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+            child: Text(
+              'Developer',
+              style: TextStyle(fontSize: 12, color: Colors.white),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 8.0),
+            child: Text(
+              'I am haz, I helped develope this app, i like tattoos and scarface. Feel free to email me if you have any enquires about our app.',
+              style: TextStyle(fontSize: 12, color: Colors.white),
             ),
           ),
           Padding(
@@ -314,31 +341,16 @@ void _launchEmail2() async {
 ),
 
 
-
-
-
-                ]),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.03,
-              ),
-            ],
-          ),
-          Positioned(
-            bottom: 10,
-            left: 20,
-            child: InkWell(
-              onTap: _launchEmail,
-              child: Text(
-                'Contact us via email',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.blue,
+                  ]),
                 ),
-              ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
+                ),
+              ],
             ),
-          ),
-        ],
+            
+          ],
+        ),
       ),
     );
   }
