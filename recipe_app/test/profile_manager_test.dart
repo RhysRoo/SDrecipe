e90fd37d52b6546fb1ca34af8179c066617a37c0
Joshua Varney <up2138017@myport.ac.uk>
2024-04-03 00:34:51 +0100

    Partial Profile Manager Tests

--- recipe_app/test/profile_manager_test.dart
+++ recipe_app/test/profile_manager_test.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_log/pages/profile_page/profileManager.dart';
 import 'package:flutter_log/pages/profile_page/userManager.dart';
@@ -8,6 +9,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 class MockUserManager extends Mock implements UserManager {}
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+
+class MockUserModel extends Mock implements UserModel {}
 
 void main() {
   late ProfileManager profileManager;
@@ -22,7 +27,198 @@ void main() {
     // Profile Manager with Mocked Dependencies
     profileManager = ProfileManager(auth: mockAuth, firestore: fakeFirestore);
     profileManager.userManager =
-        MockUserManager(); // Assuming you have a MockUserManager class
+        UserManager(auth: mockAuth, firestore: fakeFirestore);
+    profileManager.auth = mockAuth;
+    profileManager.firestore = fakeFirestore;
+  });
+
+  group('Profile Manager storeUserDetails() Tests', () {
+    test('storeUserDetails stores user details in Firestore', () async {
+      // Mock the current user
+      when(mockAuth.currentUser).thenReturn(MockUser(uid: 'dummyUid'));
+
+      // Create a UserModel instance with some dummy data
+      UserModel user = const UserModel(
+        age: "30",
+        firstName: "John",
+        foodRestriction: "None",
+        lastName: "Doe",
+        username: "johndoe123",
+      );
+
+      // Call the method under test
+      await profileManager.storeUserDetails(user);
+
+      // Retrieve the stored user details from Firestore
+      DocumentSnapshot<Map<String, dynamic>> snapshot =
+          await fakeFirestore.collection('UserDetails').doc('dummyUid').get();
+
+      // Verify that the user details are stored correctly in Firestore
+      expect(snapshot.exists, true);
+      expect(snapshot.data()?['age'], "30");
+      expect(snapshot.data()?['firstName'], 'John');
+      expect(snapshot.data()?['foodRestriction'], 'None');
+      expect(snapshot.data()?['lastName'], 'Doe');
+      expect(snapshot.data()?['username'], 'johndoe123');
+    });
+
+    test('storeUserDetails stores user details in Firestore', () async {
+      // Mock the current user
+      when(mockAuth.currentUser).thenReturn(MockUser(uid: 'dummyUid'));
+
+      // Create a UserModel instance with some dummy data
+      UserModel user = const UserModel(
+        age: "30",
+        firstName: "John",
+        foodRestriction: "None",
+        lastName: "Doe",
+        username: "johndoe123",
+      );
+
+      // Call the method under test
+      await profileManager.storeUserDetails(user);
+
+      // Get a reference to the UserDetails collection
+      CollectionReference<Map<String, dynamic>> userDetailsCollection =
+          fakeFirestore.collection('UserDetails');
+
+      // Verify that the collection exists
+      QuerySnapshot<Map<String, dynamic>> querySnapshot =
+          await userDetailsCollection.get();
+      expect(querySnapshot.docs.length, greaterThan(0));
+    });
+
+    test('Incorrect user details should return null', () async {
+      // Mock the current user
+      when(mockAuth.currentUser).thenReturn(MockUser(uid: 'dummyUid'));
+
+      // Verify the result
+      final storedDocument =
+          await fakeFirestore.collection('UserDetails').doc('dummyUid').get();
+      final storedUserData = storedDocument.data();
+
+      // Expecting storedUserData to be null because of incorrect input
+      expect(storedUserData, isNull);
+    });
+
+    test('Incorrect user details should return null', () async {
+      // Mock the current user
+      when(mockAuth.currentUser).thenReturn(null);
+
+      // Verify the result
+      final storedDocument =
+          await fakeFirestore.collection('UserDetails').doc('dummyUid').get();
+      final storedUserData = storedDocument.data();
+
+      // Expecting storedUserData to be null because of incorrect input
+      expect(storedUserData, isNull);
+    });
+
+    test('Incorrect user details should return null', () async {
+      // Mock the current user
+      when(mockAuth.currentUser).thenReturn(MockUser(uid: 'dummyUid'));
+
+      // Verify the result
+      final storedDocument =
+          await fakeFirestore.collection('UserDetails').doc(null).get();
+      final storedUserData = storedDocument.data();
+
+      // Expecting storedUserData to be null because of incorrect input
+      expect(storedUserData, isNull);
+    });
+  });
+
+  group("Profile Manager getUserDetails() Tests", () {
+    test('getCurrentUserUID returns UID when user is authenticated', () async {
+      // Set up the fake Firestore collection
+      fakeFirestore.collection('UserDetails').doc('dummyUid').set({
+        "age": 20,
+        "bio": "Hello Mate",
+        "firstName": "Josh",
+        "foodRestriction": "Vegan",
+        "lastName": "Varney",
+        "username": "jrv1234455"
+      });
+
+      when(mockAuth.currentUser).thenReturn(MockUser(uid: 'dummyUid'));
+
+      Map<String, dynamic>? result = await profileManager.getUserDetails();
+
+      // Expecting a non-empty map with user details
+      expect(result, isNotNull);
+      expect(result?['age'], 20);
+      expect(result?['bio'], 'Hello Mate');
+      expect(result?['firstName'], 'Josh');
+      expect(result?['foodRestriction'], 'Vegan');
+      expect(result?['lastName'], 'Varney');
+      expect(result?['username'], 'jrv1234455');
+    });
+
+    test('getUserDetails returns null when user is not authenticated',
+        () async {
+      // Mock the absence of current user
+      when(mockAuth.currentUser).thenReturn(null);
+
+      Map<String, dynamic>? result = await profileManager.getUserDetails();
+
+      // Expecting a null result when user is not authenticated
+      expect(result, isNull);
+    });
+
+    test('getUserDetails returns user details with different data', () async {
+      fakeFirestore.collection('UserDetails').doc('anotherUid').set({
+        "age": 25,
+        "bio": "Hi there!",
+        "firstName": "Emily",
+        "foodRestriction": "None",
+        "lastName": "Smith",
+        "username": "emily123"
+      });
+      // Mock the current user
+      when(mockAuth.currentUser).thenReturn(MockUser(uid: 'anotherUid'));
+
+      // Call the method under test
+      Map<String, dynamic>? result = await profileManager.getUserDetails();
+
+      // Expecting a non-null result with user details
+      expect(result, isNotNull);
+      expect(result?['age'], 25);
+      expect(result?['bio'], 'Hi there!');
+      expect(result?['firstName'], 'Emily');
+      expect(result?['foodRestriction'], 'None');
+      expect(result?['lastName'], 'Smith');
+      expect(result?['username'], 'emily123');
+    });
+
+    test('getUserDetails returns no data', () async {
+      // Mock the current user
+      when(mockAuth.currentUser).thenReturn(MockUser(uid: 'diffUid'));
+
+      // Call the method under test
+      Map<String, dynamic>? result = await profileManager.getUserDetails();
+
+      // Expecting a non-null result with user details
+      expect(result, isNull);
+    });
+
+    test('getUserDetails returns null as testing with null users', () async {
+      fakeFirestore.collection('UserDetails').doc(null).set({
+        "age": 25,
+        "bio": "Hi there!",
+        "firstName": "Emily",
+        "foodRestriction": "None",
+        "lastName": "Smith",
+        "username": "emily123"
+      });
+      // Mock the current user
+      when(mockAuth.currentUser).thenReturn(MockUser(uid: null));
+
+      // Call the method under test
+      Map<String, dynamic>? result = await profileManager.getUserDetails();
+
+      // Expecting a non-null result with user details
+      expect(result, isNull);
+    });
   });
 
   group('Profile Manager checkInputLength() Tests', () {
