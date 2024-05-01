c38aa9d7d0dd21640154eb531f28724d52be2fad
hazaloid <up2120303@myport.ac.uk>
2024-05-01 03:09:16 +0100

    Your commit message here

--- recipe_app/lib/pages/faqs_page/FAQ_page.dart
+++ recipe_app/lib/pages/faqs_page/FAQ_page.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: sort_child_properties_last, deprecated_member_use, file_names
-
 import 'package:flutter/material.dart';
 
-//questions and awnser set up
 class QAItem extends StatelessWidget {
   final String title;
   final List<Widget> children;
@@ -11,13 +8,13 @@ class QAItem extends StatelessWidget {
   final Color childrenColor;
 
   const QAItem({
-    super.key,
+    Key? key,
     required this.title,
     required this.children,
     this.backgroundColor = Colors.green,
     this.titleColor = Colors.black,
     this.childrenColor = Colors.white,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +22,10 @@ class QAItem extends StatelessWidget {
       title: Text(
         title,
         style: TextStyle(
-            color: titleColor, fontSize: 18.0, fontWeight: FontWeight.bold),
+          color: titleColor,
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+        ),
       ),
       children: children
           .map((child) => Padding(
@@ -33,7 +33,10 @@ class QAItem extends StatelessWidget {
                 child: Align(
                   alignment: Alignment.topLeft,
                   child: DefaultTextStyle(
-                    style: TextStyle(color: childrenColor, fontSize: 16.0),
+                    style: TextStyle(
+                      color: childrenColor,
+                      fontSize: 16.0,
+                    ),
                     child: child,
                   ),
                 ),
@@ -44,184 +47,189 @@ class QAItem extends StatelessWidget {
   }
 }
 
-// ignore: camel_case_types
-class FAQ_page extends StatelessWidget {
-  const FAQ_page({super.key});
+class FAQ_page extends StatefulWidget {
+  const FAQ_page({Key? key}) : super(key: key);
+
+  @override
+  _FAQ_pageState createState() => _FAQ_pageState();
+}
+
+class _FAQ_pageState extends State<FAQ_page> {
+  late TextEditingController _searchController;
+  List<QAItem> _allQuestions = [];
+  List<QAItem> _filteredQuestions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _allQuestions = [
+      const QAItem(
+        title: 'Q: What is the main goal of the Recipe Generator app?',
+        children: [
+          Text(
+              'A: The core concept of this system is to mitigate environmental impact by reducing food waste, aligning with sustainable practices.'),
+        ],
+      ),
+      const QAItem(
+        title: 'Q: Who are the primary users of the Recipe Generator app?',
+        children: [
+          Text(
+              'A: The primary users are registered WasteAway users who seek to reduce food waste and generate personalized recipes.'),
+        ],
+      ),
+      const QAItem(
+        title: 'Q: How does the Recipe Generation feature work?',
+        children: [
+          Text(
+              'A: Users select ingredients through the form, triggering the recipe generation process. The system generates recipes based on selected ingredients and user food restrictions.'),
+        ],
+      ),
+      const QAItem(
+        title:
+            'Q: Can I customize the generated recipes based on dietary restrictions?',
+        children: [
+          Text(
+              'A: Yes, users can set dietary preferences and restrictions, ensuring that the generated recipes align with their specific needs.'),
+        ],
+      ),
+      const QAItem(
+        title:
+            'Q: What happens if I don\'t provide enough ingredients for the generation process?',
+        children: [
+          Text(
+              'A: If not enough ingredients or ingredient types are provided, an error message will be displayed, prompting users to select more ingredients and re-initiate the process.'),
+        ],
+      ),
+      const QAItem(
+        title: 'Q: How are ingredients saved in the app?',
+        children: [
+          Text(
+              'A: Users can save ingredients through the Save Ingredients feature. The ingredient manager validates the data, and the ingredients are stored in the backend attached to the user\'s profile.'),
+        ],
+      ),
+      const QAItem(
+        title: 'Q: Can I edit my user profile details?',
+        children: [
+          Text(
+              'A: Yes, users can edit their user profile details, including name, email, password, food restrictions, and bio. The updated information is then reflected in the backend.'),
+        ],
+      ),
+      const QAItem(
+        title:
+            'Q: What happens if I enter incorrect data during profile editing?',
+        children: [
+          Text(
+              'A: If there\'s invalid data, the app displays an error message (e.g., "Invalid Name Entry" or "Invalid Password Entry"). Users correct the form and resubmit.'),
+        ],
+      ),
+      const QAItem(
+        title: 'Q: How does the app handle expired ingredients?',
+        children: [
+          Text(
+              'A: The app notifies users of expired ingredients during the recipe generation process. Users can choose to continue without expired items or halt the process to add more ingredients.'),
+        ],
+      ),
+      const QAItem(
+        title: 'Q: Can I add my own recipes to the app?',
+        children: [
+          Text(
+              'A: Yes, users can add their own recipes through the Add Own Recipes feature. The system validates the submitted details, and upon success, the custom recipe is added to the app\'s database.'),
+        ],
+      ),
+      const QAItem(
+        title:
+            'Q: What happens if I make errors while adding my own recipe?',
+        children: [
+          Text(
+              'A: If there are errors (e.g., missing fields), the app displays an error message, allowing users to review and correct their form before submission.'),
+        ],
+      ),
+      const QAItem(
+        title: 'Q: What to do if your question is not here?',
+        children: [
+          Text(
+              'A: Go to About us page and send one of the team members your question.'),
+        ],
+      ),
+    ];
+    _filteredQuestions.addAll(_allQuestions);
+    _searchController = TextEditingController();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _onSearchChanged() {
+    setState(() {
+      final query = _searchController.text.toLowerCase();
+      _filteredQuestions = _allQuestions.where((qaItem) {
+        final title = qaItem.title.toLowerCase();
+        return title.contains(query);
+      }).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Frequently Asked Questions'), //title
-        backgroundColor: Colors.green, //background
+        title: const Text('Frequently Asked Questions'),
+        backgroundColor: Colors.green,
       ),
-      body: ListView(
+      body: Column(
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  //search container
-                  color: Colors.green[700],
-                  height: MediaQuery.of(context).size.height * 0.20,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Search',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Expanded(
-                            child: SizedBox(
-                              height: 50.0,
-                              child: TextField(
-                                //text box to search (not functional)
-                                decoration: InputDecoration(
-                                  hintText: 'Search...',
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: OutlineInputBorder(),
-                                  filled: true,
-                                  fillColor: Colors.white,
-                                ),
-                                style: TextStyle(color: Colors.black),
-                                minLines: 1,
-                                maxLines: 1,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          ElevatedButton(
-                            onPressed: () {
-                              // button to search (not functional)
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.green,
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 24.0, vertical: 12.0),
-                              minimumSize: const Size(0, 50),
-                            ),
-                            child: const Text(
-                              'search', // search button
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+          Container(
+            color: Colors.green[700],
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintText: 'Search...',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    _searchController.clear();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24.0, vertical: 12.0),
+                    minimumSize: const Size(0, 50),
+                  ),
+                  child: const Text(
+                    'Clear',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-          //coad for each q and a
-          const QAItem(
-            title: 'Q: What is the main goal of the Recipe Generator app?',
-            children: [
-              Text(
-                  'A: The core concept of this system is to mitigate environmental impact by reducing food waste, aligning with sustainable practices.'),
-            ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredQuestions.length,
+              itemBuilder: (context, index) {
+                return _filteredQuestions[index];
+              },
+            ),
           ),
-          const QAItem(
-            title: 'Q: Who are the primary users of the Recipe Generator app?',
-            children: [
-              Text(
-                  'A: The primary users are registered WasteAway users who seek to reduce food waste and generate personalized recipes.'),
-            ],
-          ),
-          const QAItem(
-            title: 'Q: How does the Recipe Generation feature work?',
-            children: [
-              Text(
-                  'A: Users select ingredients through the form, triggering the recipe generation process. The system generates recipes based on selected ingredients and user food restrictions.'),
-            ],
-          ),
-          const QAItem(
-            title:
-                'Q: Can I customize the generated recipes based on dietary restrictions?',
-            children: [
-              Text(
-                  'A: Yes, users can set dietary preferences and restrictions, ensuring that the generated recipes align with their specific needs.'),
-            ],
-          ),
-          const QAItem(
-            title:
-                'Q: What happens if I don\'t provide enough ingredients for the generation process?',
-            children: [
-              Text(
-                  'A: If not enough ingredients or ingredient types are provided, an error message will be displayed, prompting users to select more ingredients and re-initiate the process.'),
-            ],
-          ),
-          const QAItem(
-            title: 'Q: How are ingredients saved in the app?',
-            children: [
-              Text(
-                  'A: Users can save ingredients through the Save Ingredients feature. The ingredient manager validates the data, and the ingredients are stored in the backend attached to the user\'s profile.'),
-            ],
-          ),
-          const QAItem(
-            title: 'Q: Can I edit my user profile details?',
-            children: [
-              Text(
-                  'A: Yes, users can edit their user profile details, including name, email, password, food restrictions, and bio. The updated information is then reflected in the backend.'),
-            ],
-          ),
-          const QAItem(
-            title:
-                'Q: What happens if I enter incorrect data during profile editing?',
-            children: [
-              Text(
-                  'A: If there\'s invalid data, the app displays an error message (e.g., "Invalid Name Entry" or "Invalid Password Entry"). Users correct the form and resubmit.'),
-            ],
-          ),
-          const QAItem(
-            title: 'Q: How does the app handle expired ingredients?',
-            children: [
-              Text(
-                  'A: The app notifies users of expired ingredients during the recipe generation process. Users can choose to continue without expired items or halt the process to add more ingredients.'),
-            ],
-          ),
-          const QAItem(
-            title: 'Q: Can I add my own recipes to the app?',
-            children: [
-              Text(
-                  'A: Yes, users can add their own recipes through the Add Own Recipes feature. The system validates the submitted details, and upon success, the custom recipe is added to the app\'s database.'),
-            ],
-          ),
-          const QAItem(
-            title:
-                'Q: What happens if I make errors while adding my own recipe?',
-            children: [
-              Text(
-                  'A: If there are errors (e.g., missing fields), the app displays an error message, allowing users to review and correct their form before submission.'),
-            ],
-          ),
-          const QAItem(
-            title:
-                'Q: What to do if your question is not here?',
-            children: [
-              Text(
-                  'A: Go to About us page and send one of the team members your question.'),
-            ],
-          ),
-          
         ],
       ),
     );
